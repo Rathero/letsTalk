5c487711c85eaf50056e9aecd02d572d002ef3a6
Rubén Ausín Trujillo <Rubn Ausn Trujillo>
2016-03-19 11:19:11 +0100

    'Prueba'

---
+++ index.js
@@ -0,0 +1,37 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect("postgres://nipkadgsmcieth:Ta3sO0fTIOKAMeBPgx9cCcfbe7@ec2-54-235-152-114.compute-1.amazonaws.com:5432/dd1l9nnvkv4cm5", function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
